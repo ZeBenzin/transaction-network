631e15b2e42f03670c0cb1c33708c77e024c30eb
Zak Brown <zakbrown93@icloud.com>
2017-03-11 09:52:36 +0000

    Produce test graph
    - Use d3 v3 and React to build test graph
    - Converted histogram example to force directed graph
    - To be used as template going forward
    - https://app.asana.com/0/283615714147351/283615714147332

--- src/components/H1BGraph/index.js
+++ src/components/H1BGraph/index.js
@@ -1,46 +1,16 @@
 import React, {Component} from 'react';
-import * as d3 from 'd3';
-import Histogram from '../Histogram';
-import '../Histogram/style.css';
+import Graph from '../Graph';
+import '../Graph/style.css';
 
-class H1BGraph extends Component {
+class EntityView extends Component {
   constructor () {
     super();
     this.state = {
-      rawData: []
+      nodes: {},
+      edges: {},
+      width: 640,
+      height: 480
     };
-  }
-
-  cleanJobs (title) {
-    title = title.replace(/[^a-z ]/gi, '');
-
-    if (title.match(/consultant|specialist|expert|prof|advis|consult/)) {
-      title = 'consultant';
-    } else if (title.match(/analyst|strateg|scien/)) {
-      title = 'analyst';
-    } else if (title.match(/manager|associate|train|manag|direct|supervis|mgr|chief/)) {
-      title = 'manager';
-    } else if (title.match(/architect/)) {
-      title = 'architect';
-    } else if (title.match(/lead|coord/)) {
-      title = 'lead';
-    } else if (title.match(/eng|enig|ening|eign/)) {
-      title = 'engineer';
-    } else if (title.match(/program/)) {
-      title = 'programmer';
-    } else if (title.match(/design/)) {
-      title = 'designer';
-    } else if (title.match(/develop|dvelop|develp|devlp|devel|deelop|devlop|devleo|deveo/)) {
-      title = 'developer';
-    } else if (title.match(/tester|qa|quality|assurance|test/)) {
-      title = 'tester';
-    } else if (title.match(/admin|support|packag|integrat/)) {
-      title = 'administrator';
-    } else {
-      title = 'other';
-    }
-
-    return title;
   }
 
   componentWillMount () {
@@ -48,63 +18,32 @@ class H1BGraph extends Component {
   }
 
   loadRawData () {
-    const formatDate = d3.time.format('%m/%d/%Y');
-    d3.csv(this.props.url)
-      .row((d) => {
-        if (!d['base salary']) return null;
-        return (
-          {
-            employer: d.employer,
-            submit_date: formatDate.parse(d['submit date']),
-            start_date: formatDate.parse(d['start date']),
-            case_status: d['case status'],
-            job_title: d['job title'],
-            clean_job_title: this.cleanJobs(d['job title']),
-            base_salary: Number(d['base salary']),
-            salary_to: d['salary to'] ? Number(d['salary to']) : null,
-            city: d.city,
-            state: d.state
-          }
-        );
-      })
-      .get((error, rows) => {
-        if (error) {
-          console.error(error);
-          console.error(error.stack);
-        } else {
-          this.setState({rawData: rows});
-        }
-      });
+    // Will eventually use JSON for this step
+    const nodes = [
+      { x: this.state.width / 3, y: this.state.height / 2 },
+      { x: 2 * this.state.width / 3, y: this.state.height / 2 }
+    ];
+    const edges = [{ source: 0, target: 1 }];
+    this.setState({nodes, edges});
+    console.log(this.state);
   }
 
   render () {
-    if (!this.state.rawData.length) {
-      return (
-        <h2>
-          Loading data about 81k H1B visas in the software industry\
-        </h2>
-      );
-    }
-
     const params = {
-      bins: 20,
-      width: 500,
-      height: 500,
-      axisMargin: 83,
-      topMargin: 10,
-      bottomMargin: 5,
-      value: d => d.base_salary
+      nodes: this.state.nodes,
+      edges: this.state.edges,
+      width: this.state.width,
+      height: this.state.height
     };
-    const fullWidth = 700;
 
     return (
       <div>
-        <svg width={fullWidth} height={params.height}>
-          <Histogram {...params} data={this.state.rawData} />
+        <svg width={params.width} height={params.height}>
+          <Graph {...params} />
         </svg>
       </div>
     );
   }
 }
 
-export default H1BGraph;
+export default EntityView;
