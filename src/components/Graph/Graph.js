87fd017c26d1d8471f47f75f62b1fa715191bd64
ZeBenzin <zakbrown93@icloud.com>
2017-03-14 20:38:55 +0000

    Simulate btc entity (#3)
    - Attempt at graph with react
    - Use D3 to render DOM
    - React used for structuring only
    - Viz calculations and rendering handled by D3

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -1,93 +1,55 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import d3 from 'd3';
+import * as d3Graph from '../D3Graph/d3Graph';
+import './style.css';
 
 class Graph extends Component {
   constructor (props) {
     super();
     this.configureForce(props);
-    this.updateD3(props);
-  }
-
-  componentWillReceive (newProps) {
-    this.updateD3(newProps);
   }
 
   configureForce (props) {
     this.force = d3.layout.force()
+      .charge(-300)
       .size([props.width, props.height])
-      .nodes(props.nodes)
-      .links(props.edges)
-      .linkDistance(props.width / 2);
+      .linkDistance(50);
   }
 
-  makeLink (link, index) {
-    const props = {
-      x1: link.source.x,
-      x2: link.target.x,
-      y1: link.source.y,
-      y2: link.target.y
-    };
-    return (
-      <Link {...props} key={index} />
-    );
+  componentDidMount () {
+    this.graph = d3.select(ReactDOM.findDOMNode(this.refs.graph));
+    this.force.on('tick', () => {
+      this.graph.call(d3Graph.updateGraph);
+    });
   }
 
-  makeNode (node, index) {
-    const props = {
-      width: this.props.width,
-      x: node.x,
-      y: node.y
-    };
-    return (
-      <Node {...props} key={index} />
-    );
-  }
+  shouldComponentUpdate (nextProps) {
+    this.graph = d3.select(ReactDOM.findDOMNode(this.refs.graph));
 
-  updateD3 (props) {
-    // this.force.on('end', this.solveMyProblems.bind(this));
+    const d3Nodes = this.graph.selectAll('.node')
+      .data(nextProps.nodes, node => node.key);
+    d3Nodes.enter().append('g').call(d3Graph.enterNode);
+    d3Nodes.exit().remove();
+    d3Nodes.call(d3Graph.updateNode);
+
+    const d3Links = this.graph.selectAll('.link')
+      .data(nextProps.links, link => link.key);
+    d3Links.enter().insert('line', '.node').call(d3Graph.enterLink);
+    d3Links.exit().remove();
+    d3Links.call(d3Graph.updateLink);
+
+    this.force.nodes(nextProps.nodes)
+      .links(nextProps.links);
     this.force.start();
-  }
-
-  solveMyProblems () {
-    this.node.attr('r', this.props.width / 25)
-      .attr('cx', d => d.x)
-      .attr('cy', d => d.y);
-
-    this.link.attr('x1', d => d.source.x)
-      .attr('y1', d => d.source.y)
-      .attr('x2', d => d.target.x)
-      .attr('y2', d => d.target.y);
+    return false;
   }
 
   render () {
     return (
-      <g className='graph'>
-        {this.props.edges.map(this.makeLink.bind(this))}
-        {this.props.nodes.map(this.makeNode.bind(this))}
-      </g>
-    );
-  }
-}
-
-class Node extends Component {
-  render () {
-    return (
-      <circle className='node'
-        r={this.props.width / 25}
-        cx={this.props.x}
-        cy={this.props.y} />
-    );
-  }
-}
-
-class Link extends Component {
-  render () {
-    return (
-      <line className='link'
-        x1={this.props.x1}
-        x2={this.props.x2}
-        y1={this.props.y1}
-        y2={this.props.y2} />
+      <svg width={this.props.width} height={this.props.height}>
+        <g ref='graph' />
+      </svg>
     );
   }
 }
