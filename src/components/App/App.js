5f6eb42df10e4fd36953e92c5ae92df3449fefd2
Zak Brown <zakbrown93@icloud.com>
2017-03-14 20:14:16 +0000

    Directory Cleanup
    - Refactors the project structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import EntityView from './components/EntityView/';
+import EntityView from '../EntityView/';
 
 class App extends Component {
   render () {
