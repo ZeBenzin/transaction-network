20a1d458c2046c426b73626a516cfc2d3e702eeb
Zak Brown <zakbrown93@icloud.com>
2017-04-16 23:37:25 +0100

    Transactions Displayed in Graph + Random Address Option
    - System capable of receiving n transactions and displaying them in graph
    - Graph styling tweaked
    - Random address option added to search section of UI

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import 'src/components/App/App.css';
 import EntityView from 'src/components/EntityView/EntityView';
-// import EntitySearchInput from 'src/components/EntitySearchInput/EntitySearchInput';
 import store from 'src/state/store/store';
 
 class App extends Component {
